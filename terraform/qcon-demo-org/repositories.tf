78eb228dd993197e2a780f215d151a6bfb4e9036
Varun Sharma <varunsh@stepsecurity.io>
2023-03-12 08:42:58 -0700

    Update repositories.tf

--- terraform/qcon-demo-org/repositories.tf
+++ terraform/qcon-demo-org/repositories.tf
@@ -3,3 +3,9 @@ module "terraform_spring-petclinic" {
   repo_name        = "spring-petclinic"
   repo_description = "A sample Spring-based application"
 }
+  
+module "terraform_repo2" {
+  source           = "../modules/github_repository"
+  repo_name        = "repo2"
+  repo_description = "Test repo2"
+}

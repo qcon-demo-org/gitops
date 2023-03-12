261c2ebf1c39c189a5b9eff5dbcda589f82634eb
Varun Sharma <varunsh@stepsecurity.io>
2023-03-12 11:37:49 -0700

    Update repositories.tf

--- terraform/qcon-demo-org/repositories.tf
+++ terraform/qcon-demo-org/repositories.tf
@@ -4,10 +4,10 @@ provider "github" {
 }
 
 resource "github_repository" "spring-petclinic" {
-  name               = "spring-petclinic"
-  description        = "A sample Spring-based application"
-  visibility         = "public"
-  auto_init          = true  
+  name        = "spring-petclinic"
+  description = "A sample Spring-based application"
+  visibility  = "public"
+  auto_init   = true  
 }
 
 resource "github_branch_protection" "branch_protection_spring-petclinic" {
